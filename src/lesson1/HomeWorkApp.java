3dd97e24be744fabd6776fad13aeb4e1de6cfd75
Evgenii <blykaa63@mail.ru>
2022-01-18 19:26:18 +0300

    Домашка1

---
+++ src/lesson1/HomeWorkApp.java
@@ -0,0 +1,45 @@
+package lesson1;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Banana");
+        System.out.println("Orange");
+        System.out.println("Apple");
+    }
+    public static void checkSumSign() {
+        int a = 10;
+        int b = 15;
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+    public static void printColor() {
+        int value = 101;
+        if (value <=0) {
+            System.out.println("Красный");
+        } else if (value <=1 | value<=100) {
+            System.out.println("Жёлтый");
+        } else if (value >=101) {
+            System.out.println("Зелёный");
+        }
+
+    }
+    public static void compareNumbers() {
+        int a = 9;
+        int b = 15;
+        if (a>=b) {
+            System.out.println("a>=b");
+        } else {
+            System.out.println("a<b");
+        }
+    }
+}
